e5227c264bb265383e3b32bcd8aed193f643776e
ZazabT <157392321+ZazabT@users.noreply.github.com>
2024-08-13 13:47:52 +0300

    working_on_backing_recipe

---
+++ backend/routes/recipeRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const route = express.Router();
+const {getAllRecipes , getRecipe} = require("../controllers/recipeController");
+
+
+// get all recipes
+route.get("/getallrecipes" , getAllRecipes)
+
+// get recipe
+route.get("/getrecipe:id" , getRecipe)
+
+
+module.exports = route
