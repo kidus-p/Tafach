29f5bf6e356cb5bd77e7652f6fe7c4d326dd4f1f
ZazabT <157392321+ZazabT@users.noreply.github.com>
2024-08-20 21:46:54 +0300

    drawer_update

---
+++ backend/middleware/upload.jsx
@@ -0,0 +1,16 @@
+const multer = require('multer');
+const path = require('path');
+
+// Configure multer storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/profile_pictures'); // Save files to this directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to filename
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
