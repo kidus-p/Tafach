4f852bf07da419d3e7b17cb8b84bfb544ee5ee9b
Kidus Petros <kidusptr@gmail.com>
2024-09-04 16:02:11 +0300

    Add 2 new pages
    Added profile and update my recipes page.

---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const profile = () => {
+  return (
+    <div>
+      <h1>going to be profile page.</h1>
+    </div>
+  );
+};
+
+export default profile;
---
+++ frontend/src/pages/UpdateRecipe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UpdateRecipe = () => {
+  return (
+    <div>
+      <h1>going to be recipe update page</h1>
+    </div>
+  );
+};
+
+export default UpdateRecipe;
